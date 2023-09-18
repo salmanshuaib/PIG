33653f792eb45317ac6833ebe390e48876d4f76e
Salman Shuaib <salman.shuaib@gmail.com>
2023-09-18 12:11:57 -0400

    py saves .xlsx as .xlsm //:sound: Echosmith

--- assemble.py
+++ assemble.py
@@ -15,17 +15,13 @@ time.sleep(2)  # Wait for the dialog to open
 # Type the new filename with .xlsm extension
 pyautogui.write('Pegasus.xlsm')
 
-# Choose the save as type for .xlsm format
-# This step might vary slightly based on your Excel version and settings.
-# For instance, in English versions of Excel, the .xlsm option may be something like the 3rd or 4th option.
-# Adjust the number of 'down' presses accordingly.
-pyautogui.hotkey('alt', 's')
-for _ in range(3):
-    pyautogui.press('down', interval=0.5)
-pyautogui.press('enter', interval=0.5)  # Select the .xlsm format
+# Navigate to the format selection using 'tab' and select macro-enabled using 'e'
+pyautogui.press('tab')
+time.sleep(0.5)
+pyautogui.press('e')
+time.sleep(0.5)
 
 # Save the file
-time.sleep(1)
 pyautogui.press('enter')
 
 # The file should now be saved as Pegasus.xlsm
