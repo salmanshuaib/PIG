7a5824e563b1f7e3c0d370d11f532cc704e387e3
Salman SHUAIB <salman.shuaib@gmail.com>
2023-10-10 17:23:39 -0400

    learn FR language: working key logger for History //:sound: Doja Cat

--- python/pig.py
+++ python/pig.py
@@ -1,5 +1,5 @@
 import os
-from pynput import keyboard
+import pyautogui
 import time
 
 # Use a string to store the captured keyboard input
@@ -21,27 +21,14 @@ def report(logs):
     with open(log_file_path, 'a') as f:  # Use 'a' (append) mode to add to the file
         f.write(logs)
 
-def on_key_press(key):
-    global key_logs
-    global end_logging_flag
-    
-    try:
-        key_name = key.char
-    except AttributeError:
-        key_name = str(key)
+print("Press '/' key to end logging.")
 
-    if key_name == end_logging_key:
+while not end_logging_flag:
+    key = pyautogui.prompt(text="Press a key (or '/' to end logging)", title="Key Logger")
+    if key == end_logging_key:
         end_logging_flag = True
     else:
-        key_logs += key_name
-
-def on_key_release(key):
-    pass
-
-# Create a listener for keyboard events
-with keyboard.Listener(on_press=on_key_press, on_release=on_key_release) as listener:
-    print("Press '/' key to end logging.")
-    listener.join()
+        key_logs += key
 
 # After logging is finished, write the key logs horizontally
 key_logs = ''.join(filter(str.isalpha, key_logs))
