67a6b9477fc35aaa0a08679706a27eae16d8a1c1
Salman SHUAIB <salman.shuaib@gmail.com>
2023-10-10 19:42:04 -0400

    Allied line interface - to assist the West //:sound: Swedish House Mafia, John Martin

--- python/pig.py
+++ python/pig.py
@@ -1,42 +1,45 @@
 import os
+import tkinter as tk
+from tkinter import simpledialog
 
 # Use a string to store the captured keyboard input
-key_logs = ""
+key_records = ""
 
-# Create a flag to indicate the end of logging
-end_logging_flag = False
+# Create a flag to indicate the end of recording
+end_recording_flag = False
 
-# Define the key combination to end logging (e.g., pressing '/' key)
-end_logging_key = "/"
+# Define the key combination to end recording (e.g., pressing '/' key)
+end_recording_key = "/"
 
-# Define the Unicode code point for the Enter key
-enter_key_unicode = '\r'
+# Define the path to the record file
+record_file_path = os.path.join(os.getcwd(), 'blackbox.txt')
 
-# Define the path to the log file
-log_file_path = os.path.join(os.getcwd(), 'blackbox.txt')
-
-def report(logs):
+def save_records(records):
     """
-    Writes the contents of the keyboard logs to a text file named 'blackbox.txt'.
+    Writes the contents of the keyboard records to a text file named 'blackbox.txt'.
     """
-    with open(log_file_path, 'a') as f:  # Use 'a' (append) mode to add to the file
-        f.write(logs + '\n')  # Append a newline character after each addition
+    with open(record_file_path, 'a') as f:  # Use 'a' (append) mode to add to the file
+        f.write(records + '\n')  # Append a newline character after each addition
 
-print("Press '/' key to end logging.")
+# Create a tkinter window (dialog box)
+root = tk.Tk()
+root.withdraw()  # Hide the main window
 
-while not end_logging_flag:
-    key = input(f"Press a key (or '{end_logging_key}' to end logging or Enter to add a newline): ")
-    if key == end_logging_key:
-        end_logging_flag = True
+print("Press '/' key to end recording.")
+
+while not end_recording_flag:
+    key = simpledialog.askstring("Ali [Allied line interface]", f"Press a key (or '{end_recording_key}' to end recording or Enter to add a newline):")
+    if key is None or key == end_recording_key:
+        end_recording_flag = True
     else:
-        if key == enter_key_unicode:
-            key_logs += '\n'  # Append a newline character when Enter is pressed
+        if key == "":
+            key_records += '\n'  # Append a newline character when Enter is pressed
         else:
-            key_logs += key + '\n'  # Append a newline character after each addition
+            key_records += key + '\n'  # Append a newline character after each addition
 
-# After logging is finished, write the key logs
-if key_logs:
-    report(key_logs)
-    print("Logging finished.")
+# After recording is finished, write the key records
+if key_records:
+    save_records(key_records)
+    print("Recording finished.")
 else:
-    print("No keyboard input logged.")
+    print("No keyboard input recorded.")
