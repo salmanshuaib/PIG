ee38dfda1c544d281eaf86d2d09f959bee025cc2
Salman SHUAIB <salman.shuaib@gmail.com>
2023-10-10 19:08:43 -0400

    FR word history ENABLED //:sound: Nicki Minaj

--- python/pig.py
+++ python/pig.py
@@ -9,6 +9,9 @@ end_logging_flag = False
 # Define the key combination to end logging (e.g., pressing '/' key)
 end_logging_key = "/"
 
+# Define the Unicode code point for the Enter key
+enter_key_unicode = '\r'
+
 # Define the path to the log file
 log_file_path = os.path.join(os.getcwd(), 'blackbox.txt')
 
@@ -22,14 +25,16 @@ def report(logs):
 print("Press '/' key to end logging.")
 
 while not end_logging_flag:
-    key = input("Press a key (or '/' to end logging): ")
+    key = input(f"Press a key (or '{end_logging_key}' to end logging or Enter to add a newline): ")
     if key == end_logging_key:
         end_logging_flag = True
     else:
-        key_logs += key + '\n'  # Append a newline character after each addition
+        if key == enter_key_unicode:
+            key_logs += '\n'  # Append a newline character when Enter is pressed
+        else:
+            key_logs += key + '\n'  # Append a newline character after each addition
 
-# After logging is finished, write the key logs horizontally
-key_logs = ''.join(filter(str.isalpha, key_logs))
+# After logging is finished, write the key logs
 if key_logs:
     report(key_logs)
     print("Logging finished.")
