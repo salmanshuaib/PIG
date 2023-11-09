96640e04643f8ce1022e10f6bb3797a6faf2e306
Salman Shuaib <salman.shuaib@gmail.com>
2023-11-09 18:44:19 -0500

    program almost nearing function

--- python/pig.py
+++ python/pig.py
@@ -1,16 +1,27 @@
 import random
 import datetime
 
+# isolate first digit after decimal point; in a number
+def crown(princes):
+    num_str = str(princes)
+    decimal_index = num_str.find('.')
+    if decimal_index != -1 and decimal_index + 1 < len(num_str):
+        return int(num_str[decimal_index + 1])
+    else:
+        return None
+
 # Function to get the current date and time as a string
 def current_date_time():
     now = datetime.datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
 # Generate a random number from 1 to 9
-king = random.randint(1, 9)
+princes = random.uniform(0.1, 1)
+emperor = 1
+kings = (crown(princes))/10
 
-pilots = 12
-print(king)
+princnes = 10
+print(kings)
 
 # Admission: Either BS can be returned for BS [OR] Surrender can be returned for BS. Pick one, Salman SHUAIB.
 def Constancy(pilots):
@@ -22,17 +33,16 @@ def written(Xfile, ans):
     TotalKnockout = Xfile
     MerciBeaucoup = ans
     # Write the date and time above each record
-    TotalKnockout.write(f"Difficulty {king} //{current_date_time()}\n")  # Fixed the call to current_date_time()
-    TotalKnockout.write(f"Input {pilots}: {MerciBeaucoup}\n")
+    TotalKnockout.write(f"Difficulty {kings} //{current_date_time()}\n")  # Fixed the call to current_date_time()
+    TotalKnockout.write(f"Input {princes}: {MerciBeaucoup}\n")
     return TotalKnockout
 
-emperor = 1
-kings = 100
+
 Xfile = open("blackbox.txt", "w")
-while emperor != king:
-    y = Constancy(pilots)
+while kings != emperor:
+    y = Constancy(princes)
     thatsY = Xfile
     written(thatsY, y)
-    emperor = kings + 1
+    kings = kings + 0.1
 
 Xfile.close()
