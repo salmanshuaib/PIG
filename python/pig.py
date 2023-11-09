7b9960dfa21cf196ebe3d7d67bb69ad3fee22373
Salman Shuaib <salman.shuaib@gmail.com>
2023-11-09 14:51:08 -0500

    poetic coding

--- python/pig.py
+++ python/pig.py
@@ -7,33 +7,32 @@ def current_date_time():
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
 # Generate a random number from 1 to 9
-random_number = random.randint(1, 9)
+king = random.randint(1, 9)
 
-counter = 1
-print(random_number)
+pilot = 1
+print(king)
 
 def Constancy():
-    for Counter in range(1, random_number):
-        if (Counter == random_number):
-            break
-        user_input = input("Enter something you have memorized: ")
-        Counter = Counter + 1
-        return user_input
+        BitchSlap = input("Enter something you have memorized: ")
+        pilot = pilot + 1
+        return BitchSlap
 
 def written(Xfile, ans):
-    mu = Xfile
-    ui = ans
+    TotalKnockout = Xfile
+    MerciBeaucoup = ans
     # Write the date and time above each record
-    mu.write(f"Difficulty {random_number} //{current_date_time}\n")
-    mu.write(f"Input {counter}: {ui}\n")
-    mu.close()
-    return mu
+    TotalKnockout.write(f"Difficulty {king} //{current_date_time}\n")
+    TotalKnockout.write(f"Input {pilot}: {MerciBeaucoup}\n")
+    TotalKnockout.close()
+    return TotalKnockout
 
+emperor = 1
 Xfile = open("blackbox.txt", "w")
-while (counter != random_number):
+while (emperor != king):
     y = Constancy()
     thatsY = Xfile
     written(thatsY, y)
+    emperor = king + 1
 
 
 Xfile.close()     
